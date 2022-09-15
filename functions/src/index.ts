aaf186471c6ecb56302e1935d717b8d7d23a8772
srikanth597 <alapatisrikanth597@gmail.com>
2022-09-15 19:51:13 +0530

    test the circleci

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,5 @@ import * as functions from "firebase-functions";
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase from srikanth!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  response.send("Hello from Firebase from srikanth!!!!!!!!!!!!!!!!!!!!!!!!!!! cool stuff");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React   from srikanth
+          Learn React   from srikanth , cool stuff
         </a>
       </header>
     </div>
